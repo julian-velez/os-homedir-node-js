50dd917944b32cf588f1b883139fe6b13dc902f2
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-03-08 09:58:07 -0500

    Update main.js

--- os-homedir/main.js
+++ os-homedir/main.js
@@ -1,3 +1,3 @@
-const os = require('node:os')
+const os = require('node:os') // Importa el módulo 'os' de Node.js, que proporciona información del sistema operativo.
 
-console.log(os.homedir())
+console.log(os.homedir()) // Llama al método 'homedir()' del módulo 'os', que devuelve el directorio principal del usuario actual.
